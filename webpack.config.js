69e7eb7fe9e60bb4b8d725971422521f859ff744
Tiago Pires <tiago.pires@me.com>
2020-01-16 11:01:07 +1000

    Hash js filenames

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     mode: 'development',
     output: {
-        filename: 'build.js',
+        filename: 'build.[contentHash].js',
         path: path.resolve(__dirname, 'dist')
     }
 }
