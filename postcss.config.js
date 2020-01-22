b03b4ab0793f59cee267a9528ddfa3f8863353e1
Tiago Pires <tiago.pires@me.com>
2020-01-22 13:08:42 +1000

    Reduce CSS for testing

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  plugins: [
-      require('autoprefixer'),
-      require('@fullhuman/postcss-purgecss')({
-          content: [
-              './src/templates/**/*.html'
-          ],
-          css: 'src/'
-      }),
-      require('cssnano')({
-          preset: ['default', {
-              discardComments: {
-                  removeAll: true,
-              },
-          }]
-      })
-  ]
-}
+	plugins: [
+		require("autoprefixer"),
+		require("@fullhuman/postcss-purgecss")({
+			content: ["./src/templates/**/*.html"],
+			css: "src/"
+		}),
+		require("cssnano")({
+			preset: [
+				"default",
+				{
+					discardComments: {
+						removeAll: true
+					}
+				}
+			]
+		})
+	]
+};
